70c0830799971ed8ea702be0d746c6a3ceca79e4
Abdullah Khetran <khetranabdullah@gmail.com>
2024-03-16 22:13:52 +0500

    added dummy data

---
+++ dummy_data.py
@@ -0,0 +1,25 @@
+from model.todo import Todo
+
+todos: list[Todo] = [
+    {
+        "id": 1,
+        "title": "lorem",
+        "description": "lorem to do"
+    },
+    {
+        "id": 2,
+        "title": "ipsum",
+        "description": "ipsum to do"
+    },
+    {
+        "id": 3,
+        "title": "dolor",
+        "description": "dolor to do"
+    },
+    {
+        "id": 4,
+        "title": "sit",
+        "description": "sit to do"
+    },
+
+]
