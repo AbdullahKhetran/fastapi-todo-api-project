3daaf3b95505709ad544bc53280bcf2a43756f49
Abdullah Khetran <khetranabdullah@gmail.com>
2024-03-18 21:37:42 +0500

    added CRUD functions

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from fastapi import FastAPI
 from dummy_data import todos
+# interacting through service layer
+from service import todo as service
 
 app = FastAPI()
 
 
 @app.get("/")
-def get_todo():
-    return todos
+def get_todos():
+    return service.get_todos()
+
+
+@app.post("/")
+def create_todo():
+    return service.create_todo()
+
+
+@app.patch("/")
+def modify_todo():
+    return service.modify_todo()
+
+
+@app.delete("/")
+def delete_todo():
+    return service.delete_todo()
 
 
 # run `uvicorn main:app`
