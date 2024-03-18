445cc336fd813fdda981c62200c8df8b1807a539
Abdullah Khetran <khetranabdullah@gmail.com>
2024-03-18 21:36:40 +0500

    added placeholder functions

---
+++ fake/todo.py
@@ -0,0 +1,17 @@
+from dummy_data import todos as data
+
+
+def get_todos():
+    return data
+
+
+def create_todo():
+    return "Todo created"
+
+
+def modify_todo():
+    return "Todo modified"
+
+
+def delete_todo():
+    return "Todo deleted"
